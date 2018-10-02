6c2382e9be3e5793c6f3593f8a5548acc6a6a9ae
chenlanqing <743633145@qq.com>
2018-10-02 09:27:57 +0800

    first commit

---
+++ src/main/java/com/demo/crud/CrudApplication.java
@@ -0,0 +1,13 @@
+package com.demo.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+}
